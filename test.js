09453ccef71897fb34bb79daf10f9af78c7c81d2
ophen22 <133403759+lalakobe@users.noreply.github.com>
2023-05-17 17:41:15 +0900

    0517 커밋

--- test.js
+++ test.js
@@ -1,22 +1,13 @@
 let fs = require('fs');
 let input = fs.readFileSync('input.txt').toString().split('\n');
-var answer = 0;
+var answer = '';
+let n = Number(input[0].split(' ')[0]);
 
-let dice1 = Number(input[0].split(' ')[0]);
-let dice2 = Number(input[0].split(' ')[1]);
-let dice3 = Number(input[0].split(' ')[2]);
-
-// 3개
-if (dice1 === dice2 && dice2 === dice3) {
-  answer = 10000 + dice1*1000;
-} else if (dice1 === dice2) {
-  answer = 1000 + dice1 * 100;
-} else if (dice2 === dice3) {
-  answer = 1000 + dice2 * 100;
-} else if (dice1 === dice3) {
-  answer = 1000 + dice1 * 100;
-} else {
-  answer = Math.max(dice1, dice2, dice3) * 100;
+for (let i=1; i<=n; i++) {
+  for (let j=1; j<=i; j++) {
+    answer += '*';
+  }
+  answer += '\n';
 }
-// 1개
-console.log(answer)
+
+console.log(answer);
